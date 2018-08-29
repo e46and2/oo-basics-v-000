e6d2d30885d462d94aa7e70a4d656d76a35647e8
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-08-29 21:19:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,8 +13,8 @@ class Shoe
   end
   
   def cobble
-    puts "Your shoe is as good as new"
-    :condition == "new"
+    puts "Your shoe is as good as new!"
+    @condition == "new"
   end
   
   
